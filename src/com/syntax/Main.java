b29b027e4a1f98855f624ebefb8d193c0e364b31
Asghar Nazir <asghar@syntaxtech.com>
2021-12-14 21:42:57 -0500

    Second commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("Batch 10 is great");
             Thread.sleep(2000);
         }
