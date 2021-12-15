80c6ec844345a173ee4719c4e8d4a85a41eb32c3
joeactuary <goldman256@gmail.com>
2021-12-15 18:00:48 -0500

    First program

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("Batch 10 is great");
+            System.out.println("Joey G wuz here");
             Thread.sleep(2000);
         }
     }
